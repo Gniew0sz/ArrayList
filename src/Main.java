14a2a01c848507469e415779bd62de8fd806c7de
miesz <mieszalontko0@o2.pl>
2025-12-01 11:09:12 +0100

    Ex.2. b)

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 
 public class Main {
     void main(String[] args) {
-        ArrayList<String> list = new  ArrayList<>();
-        list.add("James");
-        list.add("Robert");
-        list.add("Marcus");
-        list.add("Claire");
-        list.add("Greg");
-        int n  = list.size();
+        ArrayList<Integer> list = new  ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many numbers do you want me to generate? :");
+        int n  = input.nextInt();
         for(int i = 0; i < n; i++){
-            list.forEach(System.out::print);
-            System.out.println();
-            list.remove(0);
+            list.add((int)(Math.random()*10+1));
         }
+        System.out.println("The list is: ");
+        list.forEach(System.out::println);
+        System.out.println("The reversed list is: ");
+        reverseList(list);
+    }
 
+    static void reverseList(ArrayList<Integer> list){
+        for(int i = list.size()-1;i>=0; i--){
+            System.out.println(list.get(i));
+        }
     }
 }
