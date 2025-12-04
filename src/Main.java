438e4a438762ce1abe4445b02c3e3a718cba9557
miesz <mieszalontko0@o2.pl>
2025-12-04 14:22:35 +0100

    Ex.2. k)

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class Main {
 
     void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 1000000; i++) {
-            list.add((int) (Math.random() * 100 + 1));
+        for (int i = 0; i < 10; i++) {
+            list.add((int)(Math.random() * 100));
+            System.out.println(list.get(i));
         }
-        for (int i = 0; i < 1000000; i++) {
-            linkedList.add((int) (Math.random() * 100 + 1));
+        System.out.println("The second biggest number is " + findSecondLargest(list));
+
+
+    }
+    int findSecondLargest(ArrayList<Integer> list) {
+        int biggest=list.get(0);
+        int secondBiggest=list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) > biggest) {
+                biggest = list.get(i);
+            }
         }
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
-            list.remove(i);
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > secondBiggest&&list.get(i)!=biggest) {
+                secondBiggest = list.get(i);
+            }
         }
+        return secondBiggest;
     }
 }
